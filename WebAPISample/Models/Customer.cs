84692eadc4f7741745b91c19b03854620a85ab6c
erich brunner <erich.brunner@live.at>
2015-05-31 13:34:12 +0200

    fix some comments

---
+++ WebAPISample/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPISample.Models
+{
+    public class Customer
+    {
+        string Name { get; set; }
+        string Address { get; set; }
+    }
+}
