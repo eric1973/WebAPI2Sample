596036880ae70d033995361ae02dee200f92802b
erich brunner <erich.brunner@live.at>
2015-05-31 13:29:39 +0200

    initial version

---
+++ WebAPISample/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPISample.Models;
+
+namespace WebAPISample.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1.0M },
+            new Product { Id = 2, Name = "Yo-yp", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.SingleOrDefault(prod => prod.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+    }
+}
